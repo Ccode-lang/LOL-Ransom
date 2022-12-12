27cc85ae8e26f5b02efbc8453baaf2de1a417495
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-12 13:53:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+import requests,re,os
+import time
+import sys
+from os import system
+from platform import platform
+from time import sleep
+import os
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+os.system(delet)
+time.sleep(1)
+
+print("loading modules...")
+time.sleep(0.05)
+os.system(delet)
+print("lOading modules...")
+time.sleep(0.05)
+os.system(delet)
+print("loAding modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loaDing modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadIng modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadiNg modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadinG modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading mOdules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moDules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modUles...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moduLes...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modulEs...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moduleS...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modules...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading modules...")
+time.sleep(0.1)
+os.system(delet)
+print("lOading modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loAding modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loaDing modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadIng modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadiNg modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loadinG modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading Modules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading mOdules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moDules...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modUles...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moduLes...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modulEs...")
+time.sleep(0.1)
+os.system(delet)
+print("loading moduleS...")
+time.sleep(0.1)
+os.system(delet)
+print("loading modules...")
+time.sleep(1)
+os.system(delet)
+print("""
+L█
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+Lo
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+Loa█
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+Load
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+Loadin█
+""")
+time.sleep(0.1)
+os.system(delet)
+print("""
+Loading...
+""")
+time.sleep(0.3)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+print("Loading.")
+time.sleep(0.1)
+os.system(delet)
+print("Loading..")
+time.sleep(0.1)
+os.system(delet)
+print("Loading...")
+time.sleep(0.1)
+os.system(delet)
+
+print("""
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                      ██    ██    ██                                    
+                                    ██      ██  ██                                      
+                                    ██    ██    ██                                      
+                                      ██  ██      ██                                    
+                                      ██    ██    ██                                    
+                                                                                        
+                                  ████████████████████                                  
+                                  ██                ██████                              
+                                  ██                ██  ██                              
+                                  ██                ██  ██                              
+                                  ██                ██████                              
+                                    ██            ██                                    
+                                ████████████████████████                                
+                                ██                    ██                                
+                                  ████████████████████                                  
+                                                                                        
+                               
+                                ░█████╗░██████╗░░██████╗
+                                ██╔══██╗██╔══██╗██╔════╝
+                                ██║░░╚═╝██████╔╝╚█████╗░
+                                ██║░░██╗██╔═══╝░░╚═══██╗
+                                ╚█████╔╝██║░░░░░██████╔╝
+                                ░╚════╝░╚═╝░░░░░╚═════╝░                                                         
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+
+""")
+time.sleep(0.1)
+os.system(delet)
